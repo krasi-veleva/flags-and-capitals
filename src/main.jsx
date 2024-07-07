c0ecd2919094c13fde4fd6ae3b842149b49da852
krasi-veleva <krasiveleva2002@gmail.com>
2024-07-07 19:49:49 +0300

    feat: import roboto font

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -6,5 +11,5 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
