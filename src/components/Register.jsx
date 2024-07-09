aa897bb6603198bff3e56f63a2a43603d90499d6
krasi-veleva <krasiveleva2002@gmail.com>
2024-07-09 23:33:25 +0300

    feat: add Register component

---
+++ src/components/Register.jsx
@@ -0,0 +1,164 @@
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import Link from "@mui/material/Link";
+
+const TextFieldStyle = {};
+
+export default function Register() {
+  return (
+    <Container component="main" maxWidth="md">
+      <Paper
+        elevation={3}
+        sx={{
+          padding: 20,
+          marginTop: 15,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          backgroundColor: "#f7f7f7",
+          minWidth: "450px",
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          Register Page
+        </Typography>
+
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { margin: 3, width: "100%" },
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: "100%",
+          }}
+          noValidate
+          autoComplete="on"
+        >
+          <TextField
+            required
+            id="username"
+            label="Username"
+            name="username"
+            variant="outlined"
+            sx={{
+              backgroundColor: "#ffffff",
+              borderRadius: "4px",
+            }}
+          />
+
+          <TextField
+            required
+            id="email"
+            label="Email"
+            type="email"
+            name="email"
+            variant="outlined"
+            sx={{ backgroundColor: "#ffffff", borderRadius: "4px" }}
+          />
+
+          <TextField
+            required
+            id="password"
+            label="Password"
+            type="password"
+            name="password"
+            variant="outlined"
+            sx={{ backgroundColor: "#ffffff", borderRadius: "4px" }}
+          />
+
+          <TextField
+            required
+            id="repass"
+            label="Repeat Password"
+            type="password"
+            name="repass"
+            variant="outlined"
+            sx={{ backgroundColor: "#ffffff", borderRadius: "4px" }}
+          />
+
+          <Button
+            type="submit"
+            display="flex"
+            variant="contained"
+            color="primary"
+            sx={{
+              marginTop: 3,
+              marginBottom: 2,
+              padding: "12px 24px",
+              fontSize: "18px",
+              backgroundColor: "#1565c0",
+            }}
+          >
+            Submit
+          </Button>
+        </Box>
+        <Box
+          sx={{
+            marginTop: 3,
+            padding: 2,
+            backgroundColor: "#e0e0e0",
+            borderRadius: "7px",
+            textAlign: "center",
+          }}
+        >
+          <Typography variant="body2">
+            If you already have an account <br></br>
+            <Link href="/login">Login</Link>
+          </Typography>
+        </Box>
+      </Paper>
+    </Container>
+  );
+}
+//   return (
+//     <>
+//       <Typography variant="h4" component="h1" gutterBottom>
+//         Sign Up
+//       </Typography>
+
+//       <Box
+//         component="form"
+//         sx={{
+//           "& .MuiTextField-root": { m: 2, width: "55ch" },
+//         }}
+//         noValidate
+//         autoComplete="on"
+//       >
+//         <div>
+//           <TextField
+//             required
+//             id="username"
+//             label="Username"
+//             helperText="Please enter your username"
+//           />
+//           <TextField
+//             required
+//             id="email"
+//             label="Email"
+//             helperText="Please enter your email "
+//           />
+//           <TextField
+//             id="password"
+//             label="Password"
+//             type="password"
+//             //   autoComplete="current-password"
+//           />
+//           <TextField id="repass" label="Repeat Password" />
+//           <Button
+//             type="submit"
+//             fullWidth
+//             variant="contained"
+//             color="primary"
+//             sx={{ marginTop: 3, marginBottom: 2 }}
+//           >
+//             Register
+//           </Button>
+//         </div>
+//       </Box>
+//     </>
+//   );
