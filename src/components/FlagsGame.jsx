a1b16e8c8212ec3cdd42d93bbe9ff6d4c8be18e3
krasi-veleva <krasiveleva2002@gmail.com>
2024-07-09 23:36:12 +0300

    feat:  update FlagsGame component [wip]

--- src/components/FlagsGame.jsx
+++ src/components/FlagsGame.jsx
@@ -1,24 +1,60 @@
 import Button from "@mui/material/Button";
 import { Box, Container } from "@mui/material";
+import { useState, useEffect } from "react";
 
 export default function FlagsGame() {
-  return (
-    <Container maxWidth="sm">
-      <h1>Guess the flag</h1>
+  const data = {
+    questions: [
+      {
+        flagUrl: "https://flagpedia.net/data/flags/w1160/bg.webp",
+        validAnswer: "Bulgaria",
+      },
+      {
+        flagUrl: "https://flagpedia.net/data/flags/w1160/gb-eng.webp",
+        validAnswer: "England",
+      },
+      {
+        flagUrl: "https://flagpedia.net/data/flags/w1160/hr.webp",
+        validAnswer: "Croatia",
+      },
+    ],
+    countries: ["Bulgaria", "Croatia", "England", "Avganistan2"],
+  };
+  // flagImgUrl
 
-      <Box>
-        <img
-          style={{ width: 200, border: "1px solid black" }}
-          src="https://wallpapercave.com/wp/wp4205555.jpg"
-        />
-      </Box>
+  // state:
+  // score
 
-      <Button variant="contained">Hello world</Button>
-      <Button variant="contained">Hello world</Button>
-      <Button variant="contained">Hello world</Button>
-      <Button variant="contained">Hello world</Button>
+  const [score, setScore] = useState(100);
+  const [question, setQuestion] = useState({ loading: true });
 
-      <p>Score: 0</p>
-    </Container>
-  );
+  useEffect(() => {
+    // const getNewQuestion = // TODO:
+    // const newQuestion = setQuestion(getNewQuestion);
+  }, []);
+
+  if (question.loading) {
+    return <> loading </>;
+  } else {
+    return (
+      <Container maxWidth="sm">
+        <h1>Guess the flag</h1>
+
+        <Box>
+          <img
+            style={{ width: 200, border: "1px solid black" }}
+            src={question.flagUrl}
+          />
+        </Box>
+
+        {question.answers.map((answer) => (
+          <Button key={answer} variant="contained">
+            {answer}
+          </Button>
+        ))}
+
+        <p>Score: {score}</p>
+      </Container>
+    );
+  }
 }
