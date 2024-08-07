30d9dc723e99490c752f3fe1308d53d76b53130a
krasi-veleva <krasiveleva2002@gmail.com>
2024-08-07 10:50:34 +0300

    refactor: remove unused import

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Paper from "@mui/material/Paper";
 
 export default function Home() {
   const userLoggedIn = true;
