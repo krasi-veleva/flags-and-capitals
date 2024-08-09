6a660df13a361bc839e4c78a0439d2a0b36c74b3
krasi-veleva <krasiveleva2002@gmail.com>
2024-08-09 16:29:37 +0300

    feat: add Winner component

---
+++ src/components/flagsGame/winnerPage/Winner.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+export default function Winner() {
+  return (
+    <>
+      <Typography
+        variant="h4"
+        component="h1"
+        gutterBottom
+        sx={{ fontSize: "3rem", fontWeight: "bold", color: "#333" }}
+      >
+        You Win!
+      </Typography>
+      <Box sx={{ width: "100%", textAlign: "center", marginTop: 2 }}>
+        <img
+          src={"src/assets/winner.jpeg"}
+          alt="cat"
+          style={{ maxWidth: "40%", height: "auto" }}
+        />
+      </Box>
+      <Box p={2} sx={{ textAlign: "center" }}>
+        <Button component={Link} to="/" variant="contained" size="medium">
+          Play Again
+        </Button>
+      </Box>
+    </>
+  );
+}
