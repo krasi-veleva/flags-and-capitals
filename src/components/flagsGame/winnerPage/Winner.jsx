cfe3226beadeaba94a46fa8a4affdc29faa266da
krasi-veleva <krasiveleva2002@gmail.com>
2024-08-13 17:50:24 +0300

    refactor: move css to external css module

--- src/components/flagsGame/winnerPage/Winner.jsx
+++ src/components/flagsGame/winnerPage/Winner.jsx
@@ -4,16 +4,18 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
+import styles from "./Winner.module.css";
+
 export default function Winner() {
   return (
     <>
-      <Typography variant="h4" component="h1" gutterBottom sx={{ fontSize: "3rem", fontWeight: "bold", color: "#333" }}>
+      <Typography variant="h4" component="h1" gutterBottom className={styles.title}>
         You Win!
       </Typography>
-      <Box sx={{ width: "100%", textAlign: "center", marginTop: 2 }}>
-        <img src={"src/assets/winner.jpeg"} alt="cat" style={{ maxWidth: "40%", height: "auto" }} />
+      <Box className={styles.imageContainer}>
+        <img src={"src/assets/winner.jpeg"} alt="cat" className={styles.image} />
       </Box>
-      <Box display="flex" flexDirection="column" alignItems="center" gap={2} p={2} sx={{ textAlign: "center" }}>
+      <Box className={styles.buttonContainer}>
         <Button component={Link} to="/" variant="contained" size="medium">
           Play Again
         </Button>
@@ -27,3 +29,23 @@ export default function Winner() {
     </>
   );
 }
+
+// <>
+//   <Typography variant="h4" component="h1" gutterBottom sx={{ fontSize: "3rem", fontWeight: "bold", color: "#333" }}>
+//     You Win!
+//   </Typography>
+//   <Box sx={{ width: "100%", textAlign: "center", marginTop: 2 }}>
+//     <img src={"src/assets/winner.jpeg"} alt="cat" style={{ maxWidth: "40%", height: "auto" }} />
+//   </Box>
+//   <Box display="flex" flexDirection="column" alignItems="center" gap={2} p={2} sx={{ textAlign: "center" }}>
+//     <Button component={Link} to="/" variant="contained" size="medium">
+//       Play Again
+//     </Button>
+//     <Button component={Link} to="/statistic" variant="contained" size="medium">
+//       Statistics
+//     </Button>
+//     <Button component={Link} to="/" variant="contained" size="medium">
+//       Home
+//     </Button>
+//   </Box>
+// </>
--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -1,24 +1,34 @@
 import Typography from "@mui/material/Typography";
+import styles from "./NotFound.module.css";
 
 export default function NotFound() {
   return (
     <>
-      <Typography
-        variant="h4"
-        component="h1"
-        gutterBottom
-        sx={{ fontSize: "3rem", fontWeight: "bold", color: "#333" }}
-      >
+      <Typography variant="h3" component="h1" gutterBottom className={styles.title}>
         404
       </Typography>
-      <Typography
-        variant="h4"
-        component="h2"
-        gutterBottom
-        sx={{ color: "#555" }}
-      >
+      <Typography variant="h3" component="h2" gutterBottom className={styles.subtitle}>
         Page Not Found
       </Typography>
     </>
   );
 }
+
+// <>
+//   <Typography
+//     variant="h4"
+//     component="h1"
+//     gutterBottom
+//     sx={{ fontSize: "3rem", fontWeight: "bold", color: "#333" }}
+//   >
+//     404
+//   </Typography>
+//   <Typography
+//     variant="h4"
+//     component="h2"
+//     gutterBottom
+//     sx={{ color: "#555" }}
+//   >
+//     Page Not Found
+//   </Typography>
+// </>
